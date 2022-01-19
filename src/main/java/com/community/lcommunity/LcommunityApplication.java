aa8b00e5e3f51ffbad1b75d1fd8a1ab9330396a4
youzi530 <2676272310@qq.com>
2022-01-19 17:07:08 +0800

    first commit

---
+++ src/main/java/com/community/lcommunity/LcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.community.lcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcommunityApplication.class, args);
+    }
+
+}
