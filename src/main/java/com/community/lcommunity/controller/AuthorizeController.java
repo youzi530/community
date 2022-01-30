7dd21c87ae5055688d3f41766cd522001dd40b6c
youzi530 <2676272310@qq.com>
2022-01-30 17:21:45 +0800

    [lingqiao]feat:add the login session

--- src/main/java/com/community/lcommunity/controller/AuthorizeController.java
+++ src/main/java/com/community/lcommunity/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -35,6 +38,13 @@ public class AuthorizeController {
         accessTokenDTO.setState("1");
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        return "index";
+        if (user != null) {
+            //登陆成功
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败
+            return "redirect:/";
+        }
     }
 }
